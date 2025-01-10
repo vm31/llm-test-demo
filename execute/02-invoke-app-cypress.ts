3c9b68cca76a45f3ae5ce66175e7ad968c87873c
vm31 <vmiddela@gmail.com>
2025-01-10 14:10:29 +0100

    only one file askollama will work for all prompts and any requirements

--- execute/02-invoke-app-cypress.ts
+++ execute/02-invoke-app-cypress.ts
@@ -2,14 +2,20 @@ import { askOllama } from "../askOllama";
 
 const regressionSuite ='cypress/e2e/tests/swagger.regression.md';
 const outputFilePath = 'cypress/e2e/tests/swagger.spec.cy.ts';
-const customPrompt = `You are a software test engineer. Generate functional test cases for swagger applciation using Cypress in typescript based on the following requirement document:\n\n{content} Ensure the test file is formatted correctly, with all necessary braces, parentheses, and syntax. Generate only the file content without any commentary or extra text. Do not include import command "import { cy } from 'cypress';" and the word "TypeScript" in the output. 
-    
-        Example:
-        
+const customPrompt = `You are a software test engineer. Generate functional test cases for swagger applciation using Cypress in typescript based on the following requirement document:\n\n{content} Ensure the test file is formatted correctly, with all necessary braces, parentheses, and syntax. Make sure that file content does not include  Generate only the file content without any commentary or extra text and the word "TypeScript" and or import { cy } from 'cypress' in the output. 
+    Example:
+
+        import { utilityFunctions } from "../../support/utils";
+
         describe('Login Tests', () => {
+          let locators: any;
+
             beforeEach(() => {
-                cy.clearLocalStorage();
-                cy.visit('https://swagger.io/');
+                cy.visit('https://swagger.io');
+                utilityFunctions.loadLocators().then((loadedLocators) => {
+                    locators = loadedLocators;
+                 });
+                cy.contains('Sign In').should('be.visible').click();
             });
             
             it('should log in with valid credentials', () => {
@@ -21,5 +27,5 @@ const customPrompt = `You are a software test engineer. Generate functional test
 `;
 
 askOllama(regressionSuite, outputFilePath,customPrompt)
- .then(() => console.log('Test cases generated cypress test suite successfully!'))
- .catch(error => console.error('Failed to generate test cases:', error));
+    .then(() => console.log('Test cases generated cypress test suite successfully!'))
+    .catch(error => console.error('Failed to generate test cases:', error));

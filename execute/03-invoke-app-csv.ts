3986292ae425f6528bb8ee2a16450f26c5f4795f
vm31 <vmiddela@gmail.com>
2025-01-27 14:43:45 +0100

    removed unwanted files

--- execute/03-invoke-app-csv.ts
+++
@@ -1,9 +0,0 @@
-import { convertMarkdownToCsv } from "../converter";
-
-const markdownFilePath = "tests/swagger.regression.md";
-const outputFilePath = "tests/regression.tests.csv";
-
-
-convertMarkdownToCsv(markdownFilePath, outputFilePath)
-    .then(() => console.log("Conversion completed!"))
-    .catch((err) => console.error("Error:", err));
